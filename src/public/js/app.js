198f2d286ba153376d7938c6369eee40c5d25b5b
Agustin Quispe Ugarte <zeta.aguzzs@gmail.com>
2026-06-26 22:39:00 -0300

    funcionalidad iniciada

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -18,3 +18,35 @@ const paradas = await obtenerParadas();
 const lineas = await obtenerLineas();
 
 mostrarPuntos(map, puntos);
+
+const selectorOrigen = document.getElementById("origen")
+const selectorDestino = document.getElementById("destino")
+
+
+selectorDestino.disabled = true
+cargarSelector(selectorOrigen,puntos, null)
+
+function cargarSelector (selector,puntos, puntoExcluir){
+    selector.innerHTML = ""
+    const opcion = document.createElement("option")
+        opcion.textContent = "--- Selecciona un punto ---" 
+        opcion.disabled = true
+        opcion.selected = true
+        selector.appendChild(opcion)
+    puntos.forEach(punto => {
+    
+        if (punto._id === puntoExcluir) return;
+        const opcion = document.createElement("option")
+        opcion.value = punto._id
+        opcion.textContent = punto.nombre 
+        selector.appendChild(opcion)
+        
+    })
+
+}
+
+selectorOrigen.addEventListener("change", () => {
+    selectorDestino.disabled = false
+    cargarSelector(selectorDestino,puntos, selectorOrigen.value)
+})
+
