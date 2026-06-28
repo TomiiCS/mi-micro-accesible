91bf475ca63b5d8f62d53a3363841fd6ae6b81f4
Tomas Crucianelli <tomascrucianelli@gmail.com>
2026-06-28 04:46:27 -0300

    base para implementacion de buscar recorridos combinados

--- src/public/js/recorridos.js
+++ src/public/js/recorridos.js
@@ -3,6 +3,19 @@ export function buscarRecorrido(origenId, destinoId, paradas, lineas) {
     const paradasOrigen = paradas.filter (parada => parada.puntoId === origenId)
     const paradasDestino = paradas.filter (parada => parada.puntoId === destinoId)  
 
+    const recorrido = buscarRecorridoDirecto(paradasOrigen, paradasDestino, lineas)
+
+    if(recorrido) {
+        return recorrido
+    }
+    else {
+        return buscarRecorridoCombinacion(paradasOrigen, paradasDestino, paradas, lineas)
+    }
+
+}
+
+function buscarRecorridoDirecto(paradasOrigen, paradasDestino, lineas) {
+
     //busco en cada linea, si alguna tiene como parada origen y destino las que buscamos
     for(const linea of lineas) {
 
@@ -19,6 +32,7 @@ export function buscarRecorrido(origenId, destinoId, paradas, lineas) {
 
             const trayecto = linea.trayecto.slice(paradaOrigenLinea.indiceTrayecto, paradaDestinoLinea.indiceTrayecto + 1)
             return {
+                tipo: "directo",
                 linea,
                 paradaOrigen: paradaOrigenLinea,
                 paradaDestino: paradaDestinoLinea,
@@ -28,6 +42,14 @@ export function buscarRecorrido(origenId, destinoId, paradas, lineas) {
     }
 
     //no encontramos
+    return null
+
+}
+
+function buscarRecorridoCombinacion(paradasOrigen, paradasDestino, paradas, lineas) {
+    
+    //completar!
+
     return null
 
 }
